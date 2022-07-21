4233e47ee08d0136059a460289a0987d3e6c5381
ishaongithub <75757332+ishaongithub@users.noreply.github.com>
2022-07-21 10:41:06 -0700

    Add files via upload

---
+++ Add email+user.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 21 10:21:03 2022
+
+@author: ishar
+"""
+
+from tkinter import *
+root = Tk()
+root.title("Inheritance")
+root.geometry("600x600")
+root.configure(bg="turquoise")
+
+label_name = Label(root, text="User Name : ")
+label_name.place(relx=0.3,rely=0.2, anchor=CENTER)
+entry_name = Entry(root)
+entry_name.place(relx=0.6,rely=0.2, anchor=CENTER)
+label_email = Label(root, text="Email id : ")
+label_email.place(relx=0.3,rely=0.3, anchor=CENTER)
+entry_email = Entry(root)
+entry_email.place(relx=0.6,rely=0.3, anchor=CENTER)
+label = Label(root)
+
+dictionary = {}
+class Users: 
+        
+    def addUser(key, value): 
+         #access the dictionary variable inside the class
+         global dictionary
+         dictionary[key]=value
+         label["text"]=dictionary
+         #pass parameter key as the index to the dictionary and inside it assign the parameter as value.
+         #update the text parameter of the label with variable dictionary.
+        
+
+class GetUsers(Users): 
+        
+    def getUser(self):
+        username=entry_name.get()
+        email=entry_email.get()
+        Users.addUser(username,email)
+        #using get() function get the entry_name from the user and store it in variable username
+        #using the get() function get the email_id from the user and store it in variable email_id
+        #access the addUser function of the users class and inside it pass the parameters as username, email_id
+
+#Create a object of GetUsers class        
+user = GetUsers()
+btn = Button(root, text="Add user details", command=user.getUser)
+btn.place(relx=0.5,rely=0.4, anchor=CENTER)
+label.place(relx=0.5,rely=0.5, anchor=CENTER)
+root.mainloop()
